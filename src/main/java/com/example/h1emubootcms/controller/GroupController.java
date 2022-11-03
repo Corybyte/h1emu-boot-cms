8c3270f79c4e73936bba7ca12d9d264663915bc4
CoryByte <rzd200213@gmail.com>
2022-11-03 20:25:48 +0800

    123

--- src/main/java/com/example/h1emubootcms/controller/GroupController.java
+++ src/main/java/com/example/h1emubootcms/controller/GroupController.java
@@ -1,12 +1,11 @@
 package com.example.h1emubootcms.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
+import com.example.h1emubootcms.H1emuBootCmsApplication;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * @Author 写你的名字
+ * @Author CoryByte
  * @Date 2022/11/2 下午10:38 （可以根据需要修改）
  * @Version 1.0 （版本号）
  */
@@ -14,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/Group")
 public class GroupController {
 
-    @RequestMapping("/update_Mapper")
-    public String update_Mapper(@RequestParam(value="content")String content){
-        return  content;
+    @RequestMapping(value = "/update_Mapper",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public String update_Mapper(@RequestBody JSONObject jsonParam){
+        System.out.println(jsonParam.toJSONString());
+        return  null;
     }
 
 
